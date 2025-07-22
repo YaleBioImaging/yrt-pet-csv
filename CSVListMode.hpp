e71ef9d8a9d406c34cb698a45ca3552b90805c83
Yassir Najmaoui <43155068+yassirnajmaoui@users.noreply.github.com>
2025-07-22 16:50:15 -0400

    Major tweaks (#3)
    * Put everything inside yrt namespace
    
    * Adjust headers

--- CSVListMode.hpp
+++ CSVListMode.hpp
@@ -1,32 +1,35 @@
 #pragma once
 
-#include "datastruct/PluginFramework.hpp"
-#include "datastruct/projection/ListMode.hpp"
-#include "datastruct/scanner/Scanner.hpp"
+#include "yrt-pet/datastruct/PluginFramework.hpp"
+#include "yrt-pet/datastruct/projection/ListMode.hpp"
+#include "yrt-pet/datastruct/scanner/Scanner.hpp"
 
 #include <memory>
 
-class CSVListMode : public ListMode
+namespace yrt::csv
 {
-public:
-	CSVListMode(const Scanner& pr_scanner, const std::string& filename);
+	class CSVListMode : public ListMode
+	{
+	public:
+		CSVListMode(const Scanner& pr_scanner, const std::string& filename);
 
-	size_t count() const override;
-	float getProjectionValue(bin_t id) const override;
-	void setProjectionValue(bin_t id, float val) override;
-	timestamp_t getTimestamp(bin_t id) const override;
-	det_id_t getDetector1(bin_t id) const override;
-	det_id_t getDetector2(bin_t id) const override;
+		size_t count() const override;
+		float getProjectionValue(bin_t id) const override;
+		void setProjectionValue(bin_t id, float val) override;
+		timestamp_t getTimestamp(bin_t id) const override;
+		det_id_t getDetector1(bin_t id) const override;
+		det_id_t getDetector2(bin_t id) const override;
 
-	// For registering the plugin
-	static std::unique_ptr<ProjectionData>
-	    create(const Scanner& scanner, const std::string& filename,
-	           const IO::OptionsResult& pluginOptions);
-	static Plugin::OptionsListPerPlugin getOptions();
+		// For registering the plugin
+		static std::unique_ptr<ProjectionData>
+		    create(const Scanner& scanner, const std::string& filename,
+		           const io::OptionsResult& pluginOptions);
+		static plugin::OptionsListPerPlugin getOptions();
 
-private:
-	std::vector<timestamp_t> timestamp_vec;
-	std::vector<det_id_t> d1_vec;
-	std::vector<det_id_t> d2_vec;
-	std::vector<float> projValue_vec;
-};
+	private:
+		std::vector<timestamp_t> timestamp_vec;
+		std::vector<det_id_t> d1_vec;
+		std::vector<det_id_t> d2_vec;
+		std::vector<float> projValue_vec;
+	};
+}  // namespace yrt::csv
--- pybind11_csv.cpp
+++ pybind11_csv.cpp
@@ -1,5 +1,5 @@
 /*
-* This file is subject to the terms and conditions defined in
+ * This file is subject to the terms and conditions defined in
  * file 'LICENSE.txt', which is part of this source code package.
  */
 
@@ -11,12 +11,16 @@
 
 namespace py = pybind11;
 
+namespace yrt
+{
+
 void registerCSVPybind11Module(py::module& m)
 {
-	auto c = pybind11::class_<CSVListMode, ListMode>(m, "CSVListMode");
+	auto c = pybind11::class_<csv::CSVListMode, ListMode>(m, "CSVListMode");
 	c.def(py::init<const Scanner&, const std::string&>());
 }
 
 REGISTER_PROJDATA_PYBIND11("CSV", CSVListMode, registerCSVPybind11Module)
 
+}  // namespace yrt
 #endif
